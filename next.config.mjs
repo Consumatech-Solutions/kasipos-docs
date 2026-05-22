70f7e318265031d7abe5476abce6e57b73cd378a
Robert Kule <kulewakangitsirobert@gmail.com>
2026-05-22 22:25:22 +0200

    chore(config): enable rehype-pretty-code for MDX syntax highlighting

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@ import nextra from 'nextra'
 
 const withNextra = nextra({
   contentDirBasePath: '/content',
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
+  mdxOptions: {
+    rehypePrettyCodeOptions: {
+      theme: {
+        dark: 'github-dark',
+        light: 'github-light'
+      }
+    }
+  }
 })
 
 export default withNextra({
